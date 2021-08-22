03c06248ac635f545ae471aa756812230656468e
David Whipple <dwhipp88@gmail.com>
2021-08-22 16:06:43 -0600

    constructed engineer test

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,7 +1,10 @@
 const Engineer = require("../lib/Engineer");
 
 test("creates an Engineer object", () => {
-    const engineer = new Engineer("Marcus");
+  const engineer = new Engineer("Marcus");
 
-    expect(engineer.name).toBe("Marcus");
-})
+  expect.stringContaining(engineer.name.toString());
+  expect(engineer.id).toEqual(expect.any(Number));
+  expect.stringContaining(engineer.email.toString());
+  expect.stringContaining(engineer.gitHub.toString());
+});
