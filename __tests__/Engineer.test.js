fd00475edad7b31e963218013d784ca18a003379
David Whipple <dwhipp88@gmail.com>
2021-08-22 16:09:47 -0600

    passed Engineer test

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,7 +1,7 @@
 const Engineer = require("../lib/Engineer");
 
 test("creates an Engineer object", () => {
-  const engineer = new Engineer("Marcus");
+  const engineer = new Engineer("Marcus", 2, "fake@city.edu", "fake@git.hub");
 
   expect.stringContaining(engineer.name.toString());
   expect(engineer.id).toEqual(expect.any(Number));
