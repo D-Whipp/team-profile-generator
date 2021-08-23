826ea8fb12687d8b55e0fd83e035f127fd31054b
David Whipple <dwhipp88@gmail.com>
2021-08-22 18:12:06 -0600

    moved create cards to it's own separate file

--- index.js
+++ index.js
@@ -4,21 +4,7 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 
-function createCard(data) {
-  return `
-    <div class="card">
-        <div class="card-header">
-            <h2 class="card-title">${managerName}</h2>
-            <h3 class="card-title"><i class="fas fa-coffee"></i>Manager</h3>
-        </div>
-        <div class="card-body">
-            <h5 class="card-text">ID: ${managerID}</h5>
-            <h5 class="card-text">Email: ${managerEmail}</h5>
-            <h5 class="card-text">Office Number: ${managerOfficeNumber}</h5>
-        </div>
-    </div>
-    `;
-}
+
 
 inquirer
   .prompt([
